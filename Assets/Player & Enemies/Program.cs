c7b9a80c328be5232aad8856c7b6adf2d98d83de
Alby-Sus <kamisatoayato205@gmail.com>
2024-11-13 08:59:34 +0700

    Initial commit

---
+++ Assets/Player & Enemies/Program.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public GameObject winCanvas;
+
+    void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveY = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveX, moveY, 0f) * moveSpeed * Time.deltaTime;
+        transform.position += movement;
+    }
+    private Vector3 startPosition;
+
+    void Start()
+    {
+        startPosition = transform.position;
+        winCanvas.SetActive(false);
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            Debug.Log("Player bị chạm vào quái, quay về vị trí ban đầu!");
+            transform.position = startPosition;
+        }
+        if (other.gameObject.CompareTag("Goal"))
+        {
+            Debug.Log("Bạn thắng");
+            winCanvas.SetActive(true);
+        }
+    }
+}
+
---
+++ Assets/Player & Enemies/Program1.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class ObstacleController : MonoBehaviour
+{
+    public Vector3 pointA;
+    public Vector3 pointB;
+    public float speed = 3f;
+
+    private bool movingToB = true;
+
+    void Update()
+    {
+        if (movingToB)
+            transform.position = Vector3.MoveTowards(transform.position, pointB, speed * Time.deltaTime);
+        else
+            transform.position = Vector3.MoveTowards(transform.position, pointA, speed * Time.deltaTime);
+
+        if (transform.position == pointB)
+            movingToB = false;
+        else if (transform.position == pointA)
+            movingToB = true;
+    }
+}
